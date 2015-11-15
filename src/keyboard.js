6a8c57ff840dba93d59b039a2876194398b4a0e1
Jeremy Glover <jeremyglover33@gmail.com>
2015-11-15 12:49:24 -0600

    Setting up keyboard.js

---
+++ src/keyboard.js
@@ -0,0 +1,54 @@
+export default function() {
+  var enter = $(`#enter`);
+  var shift = false;
+  var capslock = false;
+
+  $(`.keys li`).click(function() {
+    var char = $(this).html();
+
+    if ($(this).hasClass(`left-shift`) || $(this).hasClass(`right-shift`)) {
+      $(`.letter`).toggleClass(`uppercase`);
+      $(`.symbol span`).toggle();
+      shift = (shift === true);
+      capslock = false;
+      return false;
+    }
+
+    if ($(this).hasClass(`capslock`)) {
+      $(`.letter`).toggleClass(`uppercase`);
+      capslock = true;
+      return false;
+    }
+
+    if ($(this).hasClass(`delete`)) {
+      var html = enter.html();
+      enter.html(html.substr(0, html.lenth - 1));
+      return false;
+    }
+
+    if ($(this).hasClass(`symbol`))
+      char = $(`span:visible`, $(this)).html();
+
+    if ($(this).hasClass(`space`))
+      char = ` `;
+
+    if ($(this).hasClass(`tab`))
+      char = `\t`;
+
+    if ($(this).hasClass(`return`))
+      char = '\n';
+
+    if ($(this).hasClass('uppercase'))
+      char = char.toUpperCase();
+
+    if (shift === true) {
+      $(`.symbol span`).toggle();
+      if (capslock === false) $(`.letter`).toggleClass('uppercase');
+
+      shift = false;
+    }
+
+    enter.html(enter.html() + char);
+
+  });
+}
